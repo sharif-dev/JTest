68fafba0645241a7d88873fc96576b4393cc8df4
mahsirat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2020-03-25 04:55:35 +0430

    Map box API refactored

--- app/src/main/java/com/example/myjingilclimatepredictor/MainActivity.java
+++ app/src/main/java/com/example/myjingilclimatepredictor/MainActivity.java
@@ -11,26 +11,27 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.mapbox.mapboxsdk.Mapbox;
 //import com.mapbox.mapboxsdk.maps.MapView;
 
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    final static String TAG = "MAPBOX_TAG";
+    final static String TAG = "SKY_TAG";
     GetMap getMap;
+    GetSky getSky;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        search for tehran:
-        searchForCity("tehran",new ArrayList<String>(), new ArrayList<ArrayList<Double>>());
+        searchForCity("tehran", new ArrayList<Feature>());
+        searchForClimate("37.8267", "-122.4233", new ArrayList<Datum__>());
 
 
 
@@ -41,19 +42,36 @@ public class MainActivity extends AppCompatActivity {
         try {
             getMap.getQueue().cancelAll(TAG);
         }catch (Exception e){
-            Log.d(TAG, "no map");
+            Log.d(TAG, "no map open");
+
+        }
+        try {
+            getSky.getQueue().cancelAll(TAG);
+        }catch (Exception e){
+            Log.d(TAG, "no sky open");
 
         }
     }
 
 
-    private void searchForCity(String searchedTerm, ArrayList<String> cityNames, ArrayList<ArrayList<Double>> cityCenters){
+
+
+    private void searchForClimate(String latitude, String longitude, ArrayList<Datum__> days){
+//        todo Sabrineh check for validity of inputs
+        GetSky.Builder builder = new GetSky.Builder();
+        builder = builder.withLatitudeAndLongitude(latitude, longitude);
+        builder = builder.withContext(getApplicationContext());
+        builder = builder.withDays(days);
+        getSky = builder.build();
+        getSky.start();
+
+    }
+    private void searchForCity(String searchedTerm, List<Feature>cities){
 //        todo: Sbrineh : check for validity of searched term
         GetMap.Builder builder = new GetMap.Builder();
         builder = builder.withQuery(searchedTerm);
         builder = builder.withContext(getApplicationContext());
-        builder = builder.withCityNames(cityNames);
-        builder = builder.withCenterClasses(cityCenters);
+        builder = builder.withCites(cities);
         getMap = builder.build();
         getMap.start();
 
