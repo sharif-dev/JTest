20586a66914ebef3dbae9ae3095ae07962fd9144
SabrinehM <Sabrinehmokhtari@yahoo.com>
2020-04-08 23:42:13 +0430

    #Main Activity Updated and next intent added

--- app/src/main/java/com/example/myjingilclimatepredictor/MainActivity.java
+++ app/src/main/java/com/example/myjingilclimatepredictor/MainActivity.java
@@ -3,74 +3,41 @@ package com.example.myjingilclimatepredictor;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.ConnectivityManager;
 
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.os.FileUtils;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity<example> extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-//    private static String example;
-//    private static Alaki ex;
-//
-//    public static void write(String []args) {
-//        try {
-//            String str = ex.toString();
-//            File newTextFile = new File("save.txt");
-//
-//            FileWriter fw = new FileWriter(newTextFile);
-//            fw.write(str);
-//            fw.close();
-//
-//        } catch (IOException iox) {
-//
-//            iox.printStackTrace();
-//        }
-//    }
-//
-//
-//    final static String TAG = "SKY_TAG";
-//    GetMap getMap;
-//    GetSky getSky;
-//
-//    public MainActivity() throws FileNotFoundException {
-//    }
     final static String TAG = "SKY_TAG";
     GetMap getMap;
-    GetSky getSky;
+//    GetSky getSky;
     ArrayList<Feature> maplist;
-    ArrayList<Datum__> skylist;
-    List<String> fad = new ArrayList<>();
+//    ArrayList<Datum__> skylist;
     Handler handler = new Handler(Looper.getMainLooper()){
         @Override
         public void handleMessage(@NonNull Message msg) {
             if (msg.what == 1){
                 UpdateCityListView(msg);
             }
-            else if (msg.what == 2 || msg.what == 3){
+            else if (msg.what == 2){
                 ToastError(msg);
             }
             else {
@@ -86,30 +53,23 @@ public class MainActivity<example> extends AppCompatActivity {
     private void UpdateCityListView(Message msg) {
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, msg.getData().getStringArrayList("CityMsg")));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedcity = (String) parent.getItemAtPosition(position);
+                Intent nextactivity = new Intent(MainActivity.this, SecondActivity.class);
+                nextactivity.putExtra("cityInfo", selectedcity);
+                startActivity(nextactivity);
+            }
+        });
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        skylist = new ArrayList<>();
-        ConfigInputText();
-//        searchForClimate("51.407", "35.7117", skylist);
-
-    }
-
-    private void ConfigInputText() {
-        final EditText citysearch = (EditText)findViewById(R.id.city_name);
-        Button searchbutton = (Button) findViewById(R.id.button5);
-        searchbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (citysearch.getText().toString().length() > 0){
-                    maplist = new ArrayList<>();
-                    searchForCity(citysearch.getText().toString(), maplist);
-                }
-            }
-        });
+        ConfigCitySearch();
+//        searchForClimate("51.407", "35.7117", new ArrayList<Datum__>());
     }
 
     @Override
@@ -121,30 +81,40 @@ public class MainActivity<example> extends AppCompatActivity {
             Log.d(TAG, "no map open");
 
         }
-        try {
-            getSky.getQueue().cancelAll(TAG);
-        }catch (Exception e){
-            Log.d(TAG, "no sky open");
-
-        }
+//        try {
+//            getSky.getQueue().cancelAll(TAG);
+//        }catch (Exception e){
+//            Log.d(TAG, "no sky open");
+//
+//        }
     }
 
-
-
-
-    private void searchForClimate(String latitude, String longitude, ArrayList<Datum__> days){
-//        todo Sabrineh check for validity of inputs
-        GetSky.Builder builder = new GetSky.Builder();
-        builder = builder.withLatitudeAndLongitude(latitude, longitude);
-        builder = builder.withContext(getApplicationContext());
-        builder = builder.withDays(days);
-        builder = builder.withHandler(handler);
-        getSky = builder.build();
-        getSky.start();
-
+    private void ConfigCitySearch() {
+        final EditText citysearch = (EditText)findViewById(R.id.city_name);
+        Button searchbutton = (Button) findViewById(R.id.button5);
+        searchbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (citysearch.getText().toString().length() > 0){
+                    maplist = new ArrayList<>();
+                    Toast.makeText(MainActivity.this, citysearch.getText().toString(), Toast.LENGTH_LONG).show();
+                    searchForCity(citysearch.getText().toString(), maplist);
+                }
+            }
+        });
     }
+
+//    private void searchForClimate(String latitude, String longitude, ArrayList<Datum__> days){
+//        GetSky.Builder builder = new GetSky.Builder();
+//        builder = builder.withLatitudeAndLongitude(latitude, longitude);
+//        builder = builder.withContext(getApplicationContext());
+//        builder = builder.withDays(days);
+//        builder = builder.withHandler(handler);
+//        getSky = builder.build();
+//        getSky.start();
+//
+//    }
     private void searchForCity(String searchedTerm, List<Feature>cities){
-//        todo: Sbrineh : check for validity of searched term
         GetMap.Builder builder = new GetMap.Builder();
         builder = builder.withQuery(searchedTerm);
         builder = builder.withContext(getApplicationContext());
@@ -154,5 +124,7 @@ public class MainActivity<example> extends AppCompatActivity {
         getMap.start();
 
     }
+
+
 
 }
